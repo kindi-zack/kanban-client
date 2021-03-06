770e9e5bf1306e7098581b22e2697ab602d36dc0
alkindi <kinyobi@pop-os.localdomain>
2021-03-06 14:25:28 +0700

    kanban package.json setting

--- client/dist/main.1e43358e.js
+++ client/dist/main.1e43358e.js
@@ -16112,7 +16112,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36931" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44127" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
